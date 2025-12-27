0c08c3f21af047796a31830cc96890a2ca60a266
Aayush Shah <aayush.shah2027@gmail.com>
2025-12-27 16:23:53 +0530

    new function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 import uvicorn
 
 app = FastAPI()
@@ -14,6 +14,15 @@ def health():
 @app.get("/hello/{name}")
 def say_hello(name: str):
     return {"greeting": f"Hello {name}"}
+
+def divide(a: int, b: int):
+    if b == 0:
+        raise HTTPException(status_code=400, detail="Division by zero not allowed")
+    return {"result": a / b}
+
+def add(a: int, b: int):
+    return {"result": a +b}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
